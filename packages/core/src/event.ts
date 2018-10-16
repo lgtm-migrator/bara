6b1d739caa3f3eaa109826b510ad0fb69ac3764b
Phạm Dương Nhật Nam <nampdn@protonmail.com>
2018-10-16 23:44:48 +0700

    feat(core): WIP on BaraEvent connection

--- packages/core/src/event.ts
+++ packages/core/src/event.ts
@@ -1,9 +1,17 @@
+import * as R from 'ramda';
+import {BaraStream} from './stream';
+
 export interface BaraEventOptions {
   id: string;
   name?: string;
+  streamIds: string[];
 }
 
-export class BaraEvent implements BaraEventOptions {
+/**
+ * BaraEvent is a way to help the BaraTrigger filter various emitted events.
+ * The class should be listen on a specific event registered by the BaraStream's `id`.
+ */
+export class BaraEvent {
   /**
    * Unique event bundle id
    *
@@ -18,11 +26,47 @@ export class BaraEvent implements BaraEventOptions {
    */
   name?: string;
 
+  private streams: BaraStream[] = [];
+  private streamIds: string[] = [];
+
   constructor(options: BaraEventOptions) {
     this.id = options.id;
+    this.name = options.name;
+    this.streamIds = options.streamIds;
+  }
+
+  /**
+   * Connect the Bara application with this event.
+   * This method should be invoked by the main Bara application,
+   * and only link with the specify Stream's `id` or stream's `name` registered by this BaraEvent.
+   */
+  public connect(streams: BaraStream[]): void {
+    const streamFilter = (stream: BaraStream) => R.contains([stream.id])([this.streamIds]);
+    R.pipe(
+      // Filter all of the streams declared in `streamIds` options when creating the BaraEvent.
+      (streams: BaraStream[]) => R.filter(streamFilter)(streams),
+      // Connect each filtered stream with this BaraEvent..
+      R.forEach((stream: BaraStream) => {
+        this.connectStream(stream);
+      }),
+    )(streams);
+  }
+
+  /**
+   * Connect this BaraEvent with a BaraStream to listen on a specific event.
+   */
+  private connectStream(stream: BaraStream): void {
+    this.streams.push(stream);
+    console.log(`Registered stream ${stream.name} with event: ${this.name}`);
   }
 }
 
-export const createEvent = (options: BaraEventOptions) => {
+/**
+ * Create a BaraEvent which will help the Trigger to execute
+ * when specific registered stream is emitted an event.
+ * @param {BaraEventOptions} options
+ * @return {BaraEvent}
+ */
+export const createEvent = (options: BaraEventOptions): BaraEvent => {
   return new BaraEvent(options);
 };
