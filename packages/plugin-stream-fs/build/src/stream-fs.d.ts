f182b77c2816a53234fd017d7d22b198daaf71aa
nampdn <nampdn@protonmail.com>
2019-02-26 17:35:16 +0700

    chore(packages): remove plugin-stream-fs

--- packages/plugin-stream-fs/build/src/stream-fs.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Create filesystem stream at specific directory path.
- * @param {string} dir Which directory to observe the events.
- * @return {BaraStream}
- */
-export declare const streamFs2: (dir: string, onEvent?: ((payload: any) => void) | undefined) => void;
-export default streamFs2;
