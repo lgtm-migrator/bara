56f28f6ded878b8141b057bc41c4df85287794b6
nampdn <nampdn@protonmail.com>
2019-03-04 21:55:42 +0700

    fix(bara): export correctly

--- packages/bara/src/index.ts
+++ packages/bara/src/index.ts
@@ -1,1 +1,14 @@
-export * from '@bara/core';
+import * as bara from '@bara/core';
+
+const {
+  register,
+  useStream,
+  useTrigger,
+  useEvent,
+  useCondition,
+  useAction,
+} = bara;
+
+export {register, useStream, useTrigger, useEvent, useCondition, useAction};
+
+export default bara;
