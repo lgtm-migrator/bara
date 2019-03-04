5cdd8200aa2832aa98e15d3b99577d504b6d3876
nampdn <nampdn@protonmail.com>
2019-03-04 21:23:28 +0700

    style(cli): reformat error

--- packages/cli/src/commands/create.ts
+++ packages/cli/src/commands/create.ts
@@ -1,37 +1,33 @@
-import {Command, flags} from '@oclif/command';
-import cli from 'cli-ux';
+import { Command, flags } from "@oclif/command";
+import cli from "cli-ux";
 
-letort;
-default letss;
-Create;
-extends Command;
-{
-  static description = 'Create new Bara application';
+export default class Create extends Command {
+  static description = "Create new Bara application";
 
-  static examples = [`$ bara create
+  static examples = [
+    `$ bara create
        Create new Bara application
-`];
+`
+  ];
 
   static flags = {
-    help: flags.help({char: 'h'}),
+    help: flags.help({ char: "h" }),
     // flag with no value (-t, --template)
     template: flags.string({
-      char: 't',
-      options: ['backend', 'frontend', 'devops'],
-      description: 'Choose a template'
+      char: "t",
+      options: ["backend", "frontend", "devops"],
+      description: "Choose a template"
     })
   };
 
-  static args = [{name: 'appName'}];
+  static args = [{ name: "appName" }];
 
-  async;
-  run();
-  {
-    const {args, flags} = this.parse(Create);
+  async run() {
+    const { args, flags } = this.parse(Create);
 
     if (args.appName && flags.template) {
       this.log(`Creating from template ${flags.template}: ${args.appName}...`);
-      cli.action.start('Cloning template repo...');
+      cli.action.start("Cloning template repo...");
       await cli.wait(1500);
       cli.action.stop();
     }
--- packages/cli/src/commands/deploy.ts
+++ packages/cli/src/commands/deploy.ts
@@ -1,30 +1,25 @@
-import {Command, flags} from '@oclif/command';
+import { Command, flags } from "@oclif/command";
 
-letort;
-default letss;
-Deploy;
-extends Command;
-{
-  static description = 'Deploy Bara application to Bara Space';
+export default class Deploy extends Command {
+  static description = "Deploy Bara application to Bara Space";
 
   static flags = {
-    help: flags.help({char: 'h'}),
+    help: flags.help({ char: "h" }),
     // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
+    name: flags.string({ char: "n", description: "name to print" }),
     // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'})
+    force: flags.boolean({ char: "f" })
   };
 
-  static args = [{name: 'file'}];
+  static args = [{ name: "file" }];
 
-  async;
-  run();
-  {
-    const {args, flags} = this.parse(Deploy);
+  async run() {
+    const { args, flags } = this.parse(Deploy);
 
-    const name = flags.name || 'world';
-    this.log(`hello ${
-        name} from /Users/nam/Projects/bara/bara/packages/cli/src/commands/deploy.ts`);
+    const name = flags.name || "world";
+    this.log(
+      `hello ${name} from /Users/nam/Projects/bara/bara/packages/cli/src/commands/deploy.ts`
+    );
     if (args.file && flags.force) {
       this.log(`you input --force and --file: ${args.file}`);
     }
