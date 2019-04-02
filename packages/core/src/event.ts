2f92a2271afbb9f09180af7829fc3c9e1b92da19
nampdn <nampdn@protonmail.com>
2019-04-02 10:37:15 +0700

    style(core): update createEventType doc

--- packages/core/src/event.ts
+++ packages/core/src/event.ts
@@ -1,8 +1,12 @@
 import {BaraTriggerConfig} from './model/trigger';
 import {slugify} from './helpers/string';
 
-export function createEvent<T>(eventName: string) {
+/**
+ * Create event type with safe name, prevent bad character and duplicate event type across different streams
+ * This will be invoked at runtime by its trigger
+ */
+export function createEventType<T>(eventType: string) {
   return (trigger: BaraTriggerConfig<T>) => {
-    return slugify(`${trigger.name}___${eventName}`);
+    return slugify(`${trigger.name}___${eventType}`);
   };
 }
