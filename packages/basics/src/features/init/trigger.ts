f26d209670f02ba35ee1219fd3fae263bb63ad67
nampdn <nampdn@protonmail.com>
2019-04-06 11:51:51 +0700

    fix(basics): remove condition useless of init

--- packages/basics/src/features/init/trigger.ts
+++ packages/basics/src/features/init/trigger.ts
@@ -11,10 +11,7 @@ import { BaraInit, ON_INITIALIZED } from './event'
 export const useInitTrigger = (onInitialized: (data: BaraInit) => void) => {
   return useTrigger<BaraInit>(() => {
     const event = useEvent<BaraInit>(ON_INITIALIZED)
-    const condition = useCondition((data, payload) => {
-      return true
-    })
     const action = useAction<BaraInit>(onInitialized)
-    return { event, condition, action }
+    return { event, action }
   })
 }
