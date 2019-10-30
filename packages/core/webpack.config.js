cc958d0cfd9fd2895f4c31aa7f08173ee0f9cefc
nampdn <nampdn@protonmail.com>
2019-10-30 16:48:26 +0700

    fix(bundle): build using webpack

---
+++ packages/core/webpack.config.js
@@ -0,0 +1,41 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.ts',
+  target: 'node',
+  mode: 'development',
+  module: {
+    exprContextCritical: false,
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: 'tsconfig.build.json',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'bara',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    globalObject: `(typeof self !== 'undefined' ? self : this)`,
+  },
+  devtool: false,
+  plugins: [
+    new webpack.SourceMapDevToolPlugin({
+      filename: 'index.js.map',
+    }),
+  ],
+}
