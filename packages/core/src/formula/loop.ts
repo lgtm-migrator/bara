ef5b01490a6d1e4b48124bc9ae4cfbbc16ece74f
nampdn <nampdn@protonmail.com>
2019-10-31 10:42:54 +0700

    feat(core): add loop formula

---
+++ packages/core/src/formula/loop.ts
@@ -0,0 +1,23 @@
+import { Formula } from './types'
+
+export const loopOf = (expr: any[], action: Formula) => async (
+  payload: any,
+): Promise<any[]> => {
+  const list: any[] = expr
+  const result: any[] = []
+  for (const item of list) {
+    result.push(await action(payload)(item))
+  }
+  return result
+}
+
+export const loopIn = (action: Formula) => async (payload: any) => {
+  const result: any[] = []
+  for (const key in payload) {
+    if (payload[key] !== undefined) {
+      const r = await action(key, payload[key])(payload)
+      result.push(r)
+    }
+  }
+  return result
+}
