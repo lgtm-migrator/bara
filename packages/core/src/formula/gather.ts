d2f4051a43c27e789be8fc6a4d0948235ea0ed8a
nampdn <nampdn@protonmail.com>
2019-10-31 10:33:08 +0700

    feat(core): add gather formulas

---
+++ packages/core/src/formula/gather.ts
@@ -0,0 +1,30 @@
+import { side } from './side'
+import { Formula } from './types'
+
+/**
+ * Gathering returning value from list of actions into single JS object
+ */
+export const gatherTo = (props: string[], ...actions: Formula[]) => async (
+  payload: any,
+) => {
+  const buffer: any[] = await side(...actions)(payload)
+  const result: any = {}
+  for (let i = 0; i < buffer.length; i += 1) {
+    result[props[i] || i] = buffer[i]
+  }
+  return result
+}
+
+export const gatherReduce = (
+  initialValue: any,
+  props: string[],
+  reducer: (accumulator: any, value: any, propName?: string) => any,
+  ...actions: Formula[]
+) => async (payload: any) => {
+  const buffer: any[] = await side(...actions)(payload)
+  let result: any = initialValue
+  for (let i = 0; i < buffer.length; i += 1) {
+    result = reducer(result, buffer[i], props[i])
+  }
+  return result
+}
