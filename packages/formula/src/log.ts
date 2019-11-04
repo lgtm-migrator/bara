682333e18e7fed0dc9754aa27aea117466f0fce3
nampdn <nampdn@protonmail.com>
2019-11-04 10:12:47 +0700

    feat(formula): add log function

---
+++ packages/formula/src/log.ts
@@ -0,0 +1,31 @@
+import { Formula } from './types'
+
+/**
+ * Log function useful for debugging and logging any payload
+ * and leave the stream intact.
+ * @param action Any formula returning value will be logged here
+ * @param payloadOnly Is show only payload
+ * @param adapter Logging adapter. @default(console)
+ * @param type Type of logging adapter. @default: 'log'
+ */
+export const log = (
+  action?: Formula | undefined | null,
+  payloadOnly: boolean = true,
+  adapter: any = console,
+  type: string = 'log',
+) => (...args: any[]) => {
+  if (payloadOnly) {
+    adapter[type](args[0])
+  } else {
+    adapter[type](...args)
+  }
+  if (typeof action === 'function') {
+    return action(...args)
+  }
+  return args
+}
+
+// const proxy = (data: any) => data
+// debug(proxy)(1, [2, 3, 4]) // ?
+// proxy(debug())(2, [3, 4, 5]) // ?
+// proxy(debug(null, false))(2, [3, 4, 5]) // ?
