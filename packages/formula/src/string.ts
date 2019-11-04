79ce016993c1e42752fb475b38b9c65ac5581775
nampdn <nampdn@protonmail.com>
2019-11-04 11:49:10 +0700

    feat(formula): add template function for building string with a template

--- packages/formula/src/string.ts
+++ packages/formula/src/string.ts
@@ -1,6 +1,46 @@
+import { Formula } from './types'
+
 /**
  * Cut out any string match search expression
  * @param expr Which character to be trimmed
  */
 export const cutString = (expr: string) => (payload: string) =>
   payload.replace(new RegExp(expr), '')
+
+/**
+ * Build a string by template.
+ * Put the replacer in the bracket to map with its key.
+ * @example
+ * console.log(
+ *  template('how {be} {who} {what}?', {
+ *    be: 'are',
+ *    who: (data: any) => data.name,
+ *    what: 'doing',
+ *  })({ name: 'Nam' }),
+ * )
+ * @param s String template
+ * @param placeholder Keys of above template to replace
+ */
+export const template = (
+  s: string,
+  placeholders: { [k: string]: any | string | Formula },
+) => (payload: any, ...rest: any[]) => {
+  let result = s
+  for (const place in placeholders) {
+    if (place in placeholders) {
+      const replacer = placeholders[place]
+      result = result.replace(
+        `{${place}}`,
+        typeof replacer === 'function' ? replacer(payload, ...rest) : replacer,
+      )
+    }
+  }
+  return result
+}
+// console.log(
+//   template('how {be} {who} {what}?', {
+//     be: 'are',
+//     who: (data: any) => data.name,
+//     what: 'doing',
+//   })({ name: 'Nam' }),
+// )
