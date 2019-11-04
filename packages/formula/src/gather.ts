53eafaa05736f5987c6bdd908483c74da4d74183
nampdn <nampdn@protonmail.com>
2019-11-04 10:55:53 +0700

    feat(formula): add gather method

--- packages/formula/src/gather.ts
+++ packages/formula/src/gather.ts
@@ -3,6 +3,7 @@ import { Formula } from './types'
 
 /**
  * Gathering returning value from list of actions into single JS object
+ * @deprecated This function will be depecated in the stable version 3.0.0
  */
 export const gatherTo = (props: string[], ...actions: Formula[]) => async (
   payload: any,
@@ -26,6 +27,27 @@ export const gatherReduce = (
   let result: any = initialValue
   for (let i = 0; i < buffer.length; i += 1) {
     result = reducer(result, buffer[i], props[i])
+  }
+  return result
+}
+
+/**
+ * Gather input object into new custom object
+ * @param keys Object keys define new object from current payload by keys
+ * @example
+ * const a = gather({
+ *  hello: () => 'world',
+ *  year: (a: number, b: number) => a + b,
+ * })(2000, 19) // {hello: 'world', year: 2019}
+ */
+export const gather = (actions: { [k: string]: Formula }) => async (
+  payload: any,
+  ...rest: any[]
+) => {
+  const result: any = {}
+  for (const key in actions) {
+    if (key in actions)
+      result[key] = await Promise.resolve(actions[key](payload, ...rest))
   }
   return result
 }
