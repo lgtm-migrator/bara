4203067843685cc0aca6415938c7d7bf25ebac6f
nampdn <nampdn@protonmail.com>
2019-11-05 10:43:12 +0700

    feat(formula): add slice and concat functioin

--- packages/formula/src/array.ts
+++ packages/formula/src/array.ts
@@ -1,3 +1,5 @@
+import { Formula } from './types'
+
 /**
  * Get first element from provided array
  */
@@ -18,3 +20,31 @@ export const takeLast = () => (payload: any[]): any => {
 export const takeNth = (num: number) => (payload: any[]): any => {
   return payload && payload.length ? payload[num] : null
 }
+
+/**
+ * Imutable slicing an array into sub-array.
+ * @param startIndex Begin
+ * @param endIndex End
+ */
+export const slice = (startIndex: number, endIndex: number) => (
+  payload: any[],
+) => {
+  const copiedPayload = [...payload]
+  return copiedPayload.slice(startIndex, endIndex)
+}
+
+/**
+ * Concat multiple result of any formula into single array
+ */
+export const concat = (arrays: Formula[]) => (payload: any, ...rest: any[]) => {
+  let concatted: any[] = []
+  for (const formula of arrays) {
+    concatted = [...concatted, ...formula(payload, ...rest)]
+  }
+  return concatted
+}
+
+// concat([(data: any) => data.a, (data: any) => data.b])({
+//   a: ['Hello'],
+//   b: ['World'],
+// }) // ?
