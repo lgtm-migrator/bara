e61ddcd2a52bdb6e2a0f481e51c08367b75a350f
nampdn <nampdn@protonmail.com>
2019-11-12 14:30:00 +0700

    feat(formula): add bindArgs formula

---
+++ packages/formula/src/utils/report.ts
@@ -0,0 +1,34 @@
+import { lensProp } from '../object'
+
+/**
+ * Report a message with the help of string templating
+ * and nested prop
+ *
+ * @example
+ *
+ * report(
+ *  'Hello {world}, {greet.message}!'
+ * )({world: 'Bara', greet: {message: "How are you?"}})
+ * // console.log: "Hello Bara, How are you?"
+ */
+export const report = (
+  message: string,
+  adapter: any = console,
+  loggerMethod: string = 'log',
+) => (payload: any, ...rest: any[]) => {
+  const replacer = message.replace(
+    /\{([A-Za-z0-9._]+)\}/g,
+    (_: string, prop: string) => lensProp(prop)(payload) || `[${prop}]`,
+  )
+  adapter[loggerMethod](replacer)
+  return [payload, ...rest]
+}
+
+// const result = report(
+//   `{hello}, this message come from {from}. {nested.chi_ld}`,
+// )({
+//   hello: 'Hi',
+//   from: 'Nam',
+//   nested: { chi_ld: 'Awesome!' },
+// })
+// result
