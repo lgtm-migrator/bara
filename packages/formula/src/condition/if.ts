4620fdb83370b8919bab26512da03749bb4ee8d7
nampdn <nampdn@protonmail.com>
2019-11-13 10:38:24 +0700

    fix(formula): if statement should return the result

--- packages/formula/src/condition/if.ts
+++ packages/formula/src/condition/if.ts
@@ -1,13 +1,18 @@
 import { Formula } from '../types'
 
+/**
+ * Check for specific condition and execute according action.
+ * @param ifStm  If condition formula
+ * @param thenStm Then statement formula
+ * @param elseStm Else statement formula
+ */
 export const ifElse = (
   ifStm: Formula,
   thenStm: Formula,
   elseStm: Formula,
 ) => async (payload: any, ...rest: any[]) => {
   if (await Promise.resolve(ifStm(payload, ...rest) || false)) {
-    await Promise.resolve(thenStm(payload, ...rest))
-  } else {
-    await Promise.resolve(elseStm(payload, ...rest))
+    return await Promise.resolve(thenStm(payload, ...rest))
   }
+  return await Promise.resolve(elseStm(payload, ...rest))
 }
