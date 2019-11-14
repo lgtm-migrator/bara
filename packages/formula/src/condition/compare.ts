a4018a1f5f47bf9a6a51cc8e80481e3f006fd7a1
nampdn <nampdn@protonmail.com>
2019-11-14 07:40:54 +0700

    fix(formula): condition eq and neq not correctly implemented

--- packages/formula/src/condition/compare.ts
+++ packages/formula/src/condition/compare.ts
@@ -4,20 +4,43 @@ export const eq = (...formulas: Formula[]) => async (
   payload: any,
   ...rest: any[]
 ) => {
-  let flag = true
-  for (const formula of formulas) {
-    flag = flag && (await Promise.resolve(formula(payload, ...rest)))
-  }
-  return flag
+  return formulas.reduce(async (acc: any, formula: any) => {
+    if (acc === null) {
+      return await Promise.resolve(formula(payload, ...rest))
+    }
+    return acc === (await Promise.resolve(formula(payload, ...rest)))
+  }, Promise.resolve(null))
 }
 
 export const neq = (...formulas: Formula[]) => async (
   payload: any,
   ...rest: any[]
 ) => {
-  let flag = false
-  for (const formula of formulas) {
-    flag = flag || (await Promise.resolve(formula(payload, ...rest)))
-  }
-  return flag
+  return formulas.reduce(async (acc: any, formula: any) => {
+    if (acc === null) {
+      return await Promise.resolve(formula(payload, ...rest))
+    }
+    return acc !== (await Promise.resolve(formula(payload, ...rest)))
+  }, Promise.resolve(null))
 }
+// export const either = (...formulas: Formula[]) => async (
+//   payload: any,
+//   ...rest: any[]
+// ) => {
+//   let flag = true
+//   for (const formula of formulas) {
+//     flag = flag && (await Promise.resolve(formula(payload, ...rest)))
+//   }
+//   return flag
+// }
+
+// export const neither = (...formulas: Formula[]) => async (
+//   payload: any,
+//   ...rest: any[]
+// ) => {
+//   let flag = false
+//   for (const formula of formulas) {
+//     flag = flag || (await Promise.resolve(formula(payload, ...rest)))
+//   }
+//   return flag
+// }
