d60dc2ebfcb07784cc95a6149dc5f1a293e9ac2b
nampdn <nampdn@protonmail.com>
2019-11-14 07:10:36 +0700

    feat(formula): change find function payload

--- packages/formula/src/array/find.ts
+++ packages/formula/src/array/find.ts
@@ -1,9 +1,10 @@
 import { Formula } from '../types'
 
 /**
- * Find single element based on provided condition
+ * Find single element based on provided condition.
+ * This will emit {element, payload} for other postprocessing.
  * @param formula
  */
 export const find = (formula: Formula) => (payload: any[], ...rest: any[]) => {
-  return payload.find((element: any) => formula(element, ...rest))
+  return payload.find((element: any) => formula({ element, payload }, ...rest))
 }
