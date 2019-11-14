e5aefbfd00e4f2b4aa6b842e18d8e2d094510309
nampdn <nampdn@protonmail.com>
2019-11-14 12:01:28 +0700

    fix(formula): fix find and compare eq operator

--- packages/formula/src/array/find.ts
+++ packages/formula/src/array/find.ts
@@ -6,14 +6,29 @@ import { Formula } from '../types'
  * @param formula Checker formula should return a boolean value.
  * @param arrayProp Specify a prop name of an array if the payload is an object.
  */
-export const find = (formula: Formula, arrayProp: string = '') => (
+export const find = (formula: Formula, arrayProp: string = '') => async (
   payload: any | any[],
   ...rest: any[]
 ) => {
-  return (arrayProp !== ''
-    ? (payload as any)[arrayProp]
-    : (payload as any[])
-  ).find((element: any) =>
-    Promise.resolve(formula({ element, payload }, ...rest)),
-  )
+  const array =
+    arrayProp !== '' ? (payload as any)[arrayProp] : (payload as any[])
+  for (const element of array) {
+    const truthy = await Promise.resolve(formula({ element, payload }, ...rest))
+    if (truthy === true) {
+      return element
+    }
+  }
+  return null
 }
+
+// import { eq } from '../condition/compare'
+// find(
+//   eq(
+//     data => {
+//       return data.element.one
+//     },
+//     data => data.element.two,
+//   ),
+// )([{ one: '2', two: '3' }, { one: '3', two: '3' }]).then(data => {
+//   data //?
+// })
