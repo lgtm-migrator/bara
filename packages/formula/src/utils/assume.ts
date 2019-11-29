b49b12a4cac7bee5cc686ac36a5c06846360a6c7
nampdn <nampdn@protonmail.com>
2019-11-29 08:20:46 +0700

    feat(formula): add assume function

---
+++ packages/formula/src/utils/assume.ts
@@ -0,0 +1,10 @@
+/**
+ * Assume a payload functionality equal to data.
+ * This function will make the clearance of bara formula flow
+ * by describing what will be used as input.
+ *
+ * @param data Mock data suppose to be input
+ */
+export const assume = (data: any) => (payload: any) => {
+  return payload !== undefined ? payload : data
+}
